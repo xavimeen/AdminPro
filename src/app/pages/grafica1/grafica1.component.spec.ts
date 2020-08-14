29c19b8a982cf56cc1acc411890498b9f934c5e1
Xavi <xaviermendoza.c@outlook.com>
2020-08-13 23:56:16 -0300

    Estructurando el proyecto y primeros componentes

---
+++ src/app/pages/grafica1/grafica1.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Grafica1Component } from './grafica1.component';
+
+describe('Grafica1Component', () => {
+  let component: Grafica1Component;
+  let fixture: ComponentFixture<Grafica1Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Grafica1Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Grafica1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
