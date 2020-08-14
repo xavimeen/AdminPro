952c03c75af14e72682d6272632a32cec4bd0dc3
Xavi <xaviermendoza.c@outlook.com>
2020-08-14 20:25:10 -0300

    añadiendo rutas y módulos independientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,19 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import {LoginComponent} from './auth/login/login.component';
-import {RegisterComponent} from './auth/register/register.component';
-import {ProgressComponent} from './pages/progress/progress.component';
-import {Grafica1Component} from './pages/grafica1/grafica1.component';
-import {NotpagefoundComponent} from './pages/notpagefound/notpagefound.component';
-import { PagesComponent } from './pages/pages.component';
+// Módulos
+import {PagesRoutingModule} from './pages/pages.routing';
+import {AuthRoutingModule} from './auth/auth.routing';
+
+import {NotpagefoundComponent} from './notpagefound/notpagefound.component';
 
 const routes: Routes= [
+
+  // path: '/dashboard' PagesRouting
+  // path: '/login' authRouting
+  // path: '/register' authRouting
   
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'grafica1', component: Grafica1Component},
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]
-  },
-  
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
 
   {path: '**', component: NotpagefoundComponent}
 
@@ -32,7 +22,11 @@ const routes: Routes= [
 
 @NgModule({
   declarations: [],
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
